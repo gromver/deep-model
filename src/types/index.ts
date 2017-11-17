1b8787fa1584bc3338d93a57e783de0bf60b46e8
Gayazov Roman <gromver5@gmail.com>
2017-11-17 10:32:55 +0300

    add type shortcuts

---
+++ src/types/index.ts
@@ -0,0 +1,17 @@
+import AnyType, { AnyTypeConfig } from './AnyType';
+import ArrayType, { ArrayTypeConfig } from './ArrayType';
+import BooleanType from './BooleanType';
+import FunctionType from './FunctionType';
+import NumberType from './NumberType';
+import ObjectType,{ ObjectTypeConfig } from './ObjectType';
+import StringType from './StringType';
+import OneOfType, { OneOfTypeConfig } from './OneOfType';
+
+export const any = (config: AnyTypeConfig): AnyType => new AnyType(config);
+export const array = (config: ArrayTypeConfig): ArrayType => new ArrayType(config);
+export const boolean = (config: AnyTypeConfig): BooleanType => new BooleanType(config);
+export const func = (config: AnyTypeConfig): FunctionType => new FunctionType(config);
+export const number = (config: AnyTypeConfig): NumberType => new NumberType(config);
+export const object = (config: ObjectTypeConfig): ObjectType => new ObjectType(config);
+export const string = (config: AnyTypeConfig): StringType => new StringType(config);
+export const oneOf = (config: OneOfTypeConfig): OneOfType => new OneOfType(config);

864685e10deb012e9b7b343897e6d7f31d4734b1
Roman Gayazov <gromver5@gmail.com>
2017-11-19 12:43:06 +0300

    cleanup

--- src/permissions/MultiplePermission.ts
+++ src/permissions/MultiplePermission.ts
@@ -1,20 +1,8 @@
 import ValueContext from '../ValueContext';
 
-// export default (permissions: [(context: ValueContext) => boolean])
-//   : (context: ValueContext) => boolean => {
-//   return (context: ValueContext): boolean => {
-//     return !permissions.some(
-//       (permission: (context: ValueContext) => boolean) => {
-//         return !permission(context);
-//       },
-//     );
-//   };
-// };
-
 export default (permissions: ((valueContext: ValueContext) => void)[])
   : (valueContext: ValueContext) => void => {
   return (valueContext: ValueContext): void => {
     permissions.forEach((permission: (context: ValueContext) => void) => permission(valueContext));
   };
 };
-

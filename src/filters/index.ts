f9facb408b0af54ab04aad4ff6ceb5f62fb3c562
Roman Gayazov <gromver5@gmail.com>
2017-11-19 08:45:19 +0300

    add index.ts

---
+++ src/filters/index.ts
@@ -0,0 +1,5 @@
+import DefaultValueFilter from './DefaultValueFilter';
+import MultipleFilter from './MultipleFilter';
+
+export const defaultValue = DefaultValueFilter;
+export const multiple = MultipleFilter;

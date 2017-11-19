2409c9812283c7dce7aeb3833ccc341ef20a731d
Roman Gayazov <gromver5@gmail.com>
2017-11-19 12:42:12 +0300

    add index.ts

---
+++ src/permissions/index.ts
@@ -0,0 +1,8 @@
+import ValueContext from '../ValueContext';
+import MultiplePermission from './MultiplePermission';
+import InScenariosPermission from './InScenariosPermission';
+import ExceptScenariosPermission from './ExceptScenariosPermission';
+
+export const multiple = MultiplePermission;
+export const inScenarios = InScenariosPermission;
+export const exceptScenarios = ExceptScenariosPermission;

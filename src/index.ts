3326528e032b41214cb81219b13ff4936e1db08e
Roman Gayazov <gromver5@gmail.com>
2017-12-14 17:05:12 +0300

    add exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import * as events from './events';
 import * as types from './types';
 import * as validators from './validators';
 import * as states from './validators/states';
+import * as utils from './validators/utils';
+import * as filters from './filters';
+import * as permissions from './permissions';
 
 export { default as Model } from './Model';
 
@@ -11,7 +15,11 @@ export { default as SetContext } from './SetContext';
 export { default as ValueContext } from './ValueContext';
 
 export {
+  events,
   types,
   validators,
   states,
+  utils,
+  filters,
+  permissions,
 };

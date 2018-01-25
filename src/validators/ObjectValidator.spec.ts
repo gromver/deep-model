1ce5060da8dec9737e8c527de24cb5f3275b7477
Gayazov Roman <gromver5@gmail.com>
2018-01-25 18:12:21 +0300

    ObjectType - add additionalProperties behavior

--- src/validators/ObjectValidator.spec.ts
+++ src/validators/ObjectValidator.spec.ts
@@ -12,6 +12,7 @@ import Model from '../Model';
 import SetContext from '../SetContext';
 import ValueContext from '../ValueContext';
 import * as t from '../types';
+import * as v from '../validators';
 
 function getTestModel(attributes?) {
   return Model.object(
@@ -113,5 +114,55 @@ describe('validate', () => {
 
     await expect(model.validate()).resolves.toBe(undefined);
     expect(model.getErrors()).toHaveLength(0);
+  });
+});
+
+describe('validate with additionalProperties', () => {
+  it('Should resolve.', async () => {
+    const model = new Model({
+      type: t.object({
+        additionalProperties: t.number({
+          validator: new PresenceValidator(),
+        }),
+        properties: {
+          foo: t.string({
+            validator: new PresenceValidator(),
+          }),
+        },
+      }),
+      value: {
+        foo: 'bar',
+        unknownProp: 1,
+      },
+    });
+
+    await expect(model.validate()).resolves.toBe(undefined);
+    expect(model.getErrors()).toHaveLength(0);
+  });
+
+  it('Should reject.', async () => {
+    const model = new Model({
+      type: t.object({
+        additionalProperties: t.number({
+          validator: v.number({
+            greaterThan: 5,
+          }),
+        }),
+        properties: {
+          foo: t.string({
+            validator: new PresenceValidator(),
+          }),
+        },
+      }),
+      value: {
+        foo: 'bar',
+        unknownProp: 1,
+      },
+    });
+
+    await expect(model.validate()).rejects.toMatchObject({
+      bindings: { attribute: undefined },
+      message: '{attribute} - object has invalid fields',
+    });
   });
 });
